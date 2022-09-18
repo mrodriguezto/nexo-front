6caf42ccbeff08b27e18d5147513bab78939a0a6
mrodriguezto <mirdor.dev@gmail.com>
2022-09-17 19:38:27 -0500

    setup: tailwind theme first setup

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    modularizeImports: {
-      '@mui/material': {
-        transform: '@mui/material/{{member}}',
-      },
-      '@mui/icons-material/?(((\\w*)?/?)*)': {
-        transform: '@mui/icons-material/{{ matches.[1] }}/{{member}}',
-      },
-    },
-  },
 }
 
 module.exports = nextConfig
